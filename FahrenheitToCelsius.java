491e66b2788f452fd7418a154faea8d07985174c
Sumesh Mohanty <sumeshmohanty@gmail.com>
2022-09-11 16:51:14 +0530

    add: File FahrenheitToCelsius.java

---
+++ FahrenheitToCelsius.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.text.DecimalFormat;
+
+class FahrenheitToCelsius {
+  public static void main(String[] args) {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    float tempF, tempC; 
+    System.out.println("Program for Converting Fahrenheit To Celsius in Java!!");
+    
+    try {
+      System.out.print("Enter Temperature In Fahrenheit: ");
+      tempF = Float.parseFloat(br.readLine());
+      System.out.println(tempF);
+      tempC = (tempF - 32)/1.8F;
+      System.out.println("Temperature In Celsius: " + tempC);
+    } catch(IOException ioe) {
+      System.out.println("Something went wrong!!");
+    }
+  }
+}
