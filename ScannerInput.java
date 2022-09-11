b323bb9afd34b3cd4737a00eb342653cbbec2297
Sumesh Mohanty <sumeshmohanty@gmail.com>
2022-09-11 14:21:41 +0530

    chg: Optimized ScannerInput.java

--- ScannerInput.java
+++ ScannerInput.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.io.*;
 
 class ScannerInput {
   public static void main(String[] args) {
@@ -15,5 +14,6 @@ class ScannerInput {
     System.out.print("\n\nEnter Any Number: ");
     num = sc.nextInt();
     System.out.print("The entered number is " + num);
+    sc.close();
   }
 }
