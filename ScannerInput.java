4c15ad5fdeb9ec1b8d95cd6d0e00f39c4ec32771
Sumesh Mohanty <sumeshmohanty@gmail.com>
2022-09-11 13:20:22 +0530

    add: File ScannerInput.java

---
+++ ScannerInput.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.io.*;
+
+class ScannerInput {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String name;
+    int num;
+    System.out.println("Program for User Input in Java using Scanner!");
+    
+    System.out.print("\nEnter Your Name: ");
+    name = sc.next();
+    System.out.print("Your name is " + name);
+
+    System.out.print("\n\nEnter Any Number: ");
+    num = sc.nextInt();
+    System.out.print("The entered number is " + num);
+  }
+}
