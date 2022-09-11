41206ee84223205fdbba6fc5c4b51b9e4a51e087
Sumesh Mohanty <sumeshmohanty@gmail.com>
2022-09-11 13:03:11 +0530

    add: File PrintInteger.java

---
+++ PrintInteger.java
@@ -0,0 +1,7 @@
+class PrintInteger {
+  public static void main(String[] args) {
+    int x = 5;
+    System.out.println("Program for printing an integer in Java!!");
+    System.out.print("Value Of Integer: " + x);
+  }
+}
