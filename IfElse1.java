319a27070bfac06b2bf2558826fc0c1e2870f32d
SumeMonty <co2020.sumesh.mohanty@ves.ac.in>
2022-10-14 11:10:03 +0530

    add: File IfElse1.java

---
+++ IfElse1.java
@@ -0,0 +1,11 @@
+public class IfElse1 {
+    public static void main(String[] args) {
+        boolean isMoving = false;
+        if (isMoving) {
+            System.out.println("The bicycle is still moving!");
+        } else {
+            System.out.println("The bicycle has already stopped!");
+        } 
+
+    }
+}
